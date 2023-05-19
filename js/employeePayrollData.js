76ad758168e3f639513b2021c5639d47f1b76c7e
shephali09 <hiremathshephali9@gmail.com>
2023-05-19 11:57:21 +0530

    Add: Modify Employee Payroll Class with new Attributes and Getters and Setters

--- js/employeePayrollData.js
+++ js/employeePayrollData.js
@@ -59,4 +59,14 @@ class EmployeePayrollData {
         }
         this._startDate = startDate;
     }
+
+    //method
+    toString(){
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const empDate=!this._startDate ? "undefined" :
+        this.startDate.toLocalDateString("en-US",options);
+        return "id=" + this.id + ",name='" +this.name + ", gender='" + this.gender +
+        ",profilePic='" + this.profilePic + ", department=" + this.department +
+        ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
+    }
 }
